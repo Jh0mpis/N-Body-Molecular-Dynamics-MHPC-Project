b5d2b5391c840a43f9284b002ee935d493602d95
Jh0mpis <jmorenotr@unal.edu.co>
2024-11-24 22:09:14 +0100

    spliting the deallocating functions in files - For best modularity - main.c, include/cleanup.h and include/cleanup.c

--- include/cleanup.h
+++ include/cleanup.h
@@ -1,4 +1,10 @@
 #ifndef CLEANUP_H
 #define CLEANUP_H
+
 #include <stdio.h>
+#include "../include/mdsys.h"   // Struct definition
+
+// Function for deallocate the allocated arrays
+void clean(mdsys_t *sys, FILE **erg, FILE **traj);
+
 #endif  /*CLEANUP_H */ 
---
+++ src/cleanup.c
@@ -0,0 +1,22 @@
+#include "../include/cleanup.h"   // cleaning up function header
+#include <stdlib.h>
+
+// Deallocate and close the file
+void clean(mdsys_t *sys, FILE **erg, FILE **traj){
+  
+    // Closing the files
+    fclose(*erg);
+    fclose(*traj);
+
+    // Deallocating the arrays
+    free(sys->rx);
+    free(sys->ry);
+    free(sys->rz);
+    free(sys->vx);
+    free(sys->vy);
+    free(sys->vz);
+    free(sys->fx);
+    free(sys->fy);
+    free(sys->fz);
+
+}
