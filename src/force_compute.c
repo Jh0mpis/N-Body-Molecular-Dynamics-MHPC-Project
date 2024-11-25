007a1a9ca0e1de17b4dbe91fd5e061bec39c5f0e
bellou <rand@random.it>
2024-11-25 14:41:59 +0100

    updated the force function-to incoprate Newtons third law of motion- source folder

--- src/force_compute.c
+++ src/force_compute.c
@@ -22,8 +22,8 @@ void force(mdsys_t *sys) {
     azzero(sys->fy, sys->natoms);
     azzero(sys->fz, sys->natoms);
 
-    for (i = 0; i < sys->natoms; ++i) {
-        for (j = 0; j < sys->natoms; ++j) {
+    for (i = 0; i < (sys->natoms)-1; ++i) {
+        for (j = i+1; j < (sys->natoms); ++j) {
 
             /* Skip self-interaction */
             if (i == j) continue;
@@ -39,12 +39,12 @@ void force(mdsys_t *sys) {
                 ffac = -4.0 * sys->epsilon * (-12.0 * pow(sys->sigma / r, 12.0) / r
                                               + 6.0 * pow(sys->sigma / r, 6.0) / r);
 
-                sys->epot += 0.5 * 4.0 * sys->epsilon * (pow(sys->sigma / r, 12.0)
+                sys->epot += 4.0 * sys->epsilon * (pow(sys->sigma / r, 12.0)
                                                          - pow(sys->sigma / r, 6.0));
 
-                sys->fx[i] += rx / r * ffac;
-                sys->fy[i] += ry / r * ffac;
-                sys->fz[i] += rz / r * ffac;
+                sys->fx[i] += rx / r * ffac; sys->fx[j] -= rx / r * ffac;    
+                sys->fy[i] += ry / r * ffac; sys->fy[j] -= ry / r * ffac;   
+                sys->fz[i] += rz / r * ffac; sys->fz[j] -= rz / r * ffac;
             }
         }
     }
