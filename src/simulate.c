635447d680caa58abcd7e4201894ef2f6bd5d963
andrebenevides <andrefbenevides@gmail.com>
2024-11-25 16:16:08 +0100

    modified the run function into run_omp - to use the omp versions of force and verlet - simulation.c and simulation.h

--- src/simulate.c
+++ src/simulate.c
@@ -1,9 +1,45 @@
+#include <omp.h>
 #include "../include/simulate.h"                  // run function header file
 #include "../include/mdsys.h"                     // struct function header file
 #include "../include/force_compute.h"             // Force function header file
 #include "../include/utilities.h"                 // azzero, pbc and wallclock header file
 #include "../include/output.h"                    // output function header file
 #include "../include/verlet_time_integration.h"   // velverlet function header file
+
+
+// Principal loop function
+void run_omp(mdsys_t *sys, FILE **erg, FILE **traj, const double initial_time, const int nprint) {
+    double t_start;
+
+    /* Initialize forces and energies */
+    sys->nfi = 0;
+    force_omp(sys);
+    ekin_omp(sys);
+
+    // Printing the latency time
+    printf("Startup time: %10.3fs\n", wallclock() - initial_time);
+    printf("Starting simulation with %d atoms for %d steps.\n", sys->natoms, sys->nsteps);
+    printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
+    output(sys, *erg, *traj);
+
+    /* Reset timer */
+    t_start = wallclock();
+
+    /* Main MD loop */
+    for (sys->nfi = 1; sys->nfi <= sys->nsteps; ++sys->nfi) {
+
+        /* Write output, if requested */
+        if ((sys->nfi % nprint) == 0)
+            output(sys, *erg, *traj);
+
+        /* Propagate system and recompute energies */
+        velverlet_omp(sys);
+        ekin_omp(sys);
+    }
+
+    /* Clean up: close files, free memory */
+    printf("Simulation Done. Run time: %10.10fs\n", wallclock() - t_start);
+}
 
 // Principal loop function
 void run(mdsys_t *sys, FILE **erg, FILE **traj, const double initial_time, const int nprint){
