eae55fd8c75ee387e6e2c4389d39789619bce475
andrebenevides <andrefbenevides@gmail.com>
2024-11-25 16:18:17 +0100

    added the variable nthreads and set the number of threads in main - for parallel computing with omp - main.c

--- main.c
+++ main.c
@@ -16,6 +16,22 @@ int main(int argc, char **argv) {
     FILE *traj, *erg;
     mdsys_t sys;
     double t_start;
+    int nthreads;
+
+    /*if (argc != 2){
+        printf("Usage: %s <number_of_threads>\n", argv[0]);
+        printf("Defaulting to 1 thread.\n");
+        nthreads = 1;
+    } else {
+        nthreads = atoi(argv[1]);
+        if (nthreads <= 0) {
+            printf("Invalid number of threads specified. Defaulting to 1 thread.\n");
+            nthreads = 1;
+        }
+    }*/
+    nthreads = 1;
+    // Set the number of threads in sys
+    omp_set_num_threads(nthreads);
 
     printf("LJMD version %3.3f\n", LJMD_VERSION);
 
@@ -26,7 +42,7 @@ int main(int argc, char **argv) {
     if (nprint <= 0 ) return 1;
 
     // Run the entire simulation
-    run(&sys, &erg, &traj, t_start, nprint);
+    run_omp(&sys, &erg, &traj, t_start, nprint);
 
     // Deallocate
     clean(&sys, &erg, &traj);
