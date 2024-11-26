f4ee45c23330201a777abe748b2af58c4319a513
Jh0mpis <jmorenotr@unal.edu.co>
2024-11-26 09:08:09 +0100

    Adding the openMPI new attributes to the sys struct

--- include/mdsys.h
+++ include/mdsys.h
@@ -1,11 +1,17 @@
 #ifndef MDSYS_H
 #define MDSYS_H
+  #ifdef ENABLE_OPENMPI
+    #include <mpi.h>
+  #endif
 
 // Principal struct, defines the system properties
 typedef struct {
     double dt, mass, epsilon, sigma, box, rcut;
     double ekin, epot, temp;
     int natoms, nfi, nsteps;
+    #ifdef ENABLE_OPENMPI
+      int rank, nps, local_size, offset;
+    #endif
     double *rx, *ry, *rz;
     double *vx, *vy, *vz;
     double *fx, *fy, *fz;
