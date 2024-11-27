253b583245ee99cc9c9689bde52b351ba87d679a
bellou <rand@random.it>
2024-11-27 15:15:29 +0100

    immplemeneted a function to read the results and store them to a vector container

--- tests/test_results.cpp
+++ tests/test_results.cpp
@@ -1,184 +1,108 @@
 
 #include "gtest/gtest.h"
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<sstream>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+#include <iomanip>
+/*Error tolerance*/
+const double tol = 10E-6;
+
+void get_results(std::vector<double> &data, std::string filename)
+{
+
+
+    std::ifstream file(filename);
+
+    std::string line;
+
+
+    /* Reading the results data*/
+    if (!file.is_open())
+    {
+        std::cerr << "There has been an issue opening the file" << filename << std::endl;
+    }
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        double value;
+        while (iss >> value)
+        {
+            data.push_back(value);
+        }
+    }
+    file.close();
+}
+
 /* Test the result for 108 atoms*/
 TEST(OUTPUT_RESULT, REFRENCE_RESULT)
 {
-    double tol = 10E-3;
-    std::string results_filename = "../examples/argon_108.dat";
-    std::string reference_filename = "../reference/argon_108.dat";
-
-    std::ifstream results_file(results_filename);
-    std::ifstream reference_file(reference_filename);
-
-
-    std::string results_line; 
-    std::string reference_line;
 
     std::vector<double> results_data;
     std::vector<double> reference_data;
 
-
-
-    /* Reading the results data*/
-    if(!results_file.is_open()){
-        std::cerr<<"There has been an issue opening the file"<<results_filename<<std::endl;
-    }
-    while(std::getline(results_file, results_line)){
-        std::istringstream iss(results_line);
-        double value;
-        while(iss >> value){
-            results_data.push_back(value);
-        }
-    }
-    results_file.close();
-
-    /*Reading the refrence data*/
-    if(!reference_file.is_open()){
-        std::cerr<<"There has been an issue opening the file"<<reference_filename<<std::endl;
-    }
-    while(std::getline(reference_file, reference_line)){
-        std::istringstream iss(reference_line);
-        double value;
-        while(iss >> value){
-            reference_data.push_back(value);
-        }
-    }
-    reference_file.close();
+    get_results(results_data, "../examples/argon_108.dat");
+    get_results(reference_data, "../reference/argon_108.dat");
 
     ASSERT_EQ(reference_data.size(), results_data.size());
-    size_t nrows{(reference_data.size() / 5) / 5000}; /*Checking the first 2000 rows*/
+    size_t nrows = (reference_data.size() / 5) / 5000; /*Checking the first 2000 rows*/
     int ncols{5};
-    for(int i=0; i <  nrows;i++){
+    for (int i = 0; i < nrows; i++)
+    {
         for (int j = 0; j < ncols; j++)
         {
-            EXPECT_NEAR(reference_data[i * 5 + j], results_data[i * 5 + j], tol)
-            <<"There tests fail at index i: "<<i<<" and j: "<<j;
+            EXPECT_EQ(reference_data[i * 5 + j], results_data[i * 5 + j]);
         }
-        
     }
 }
 /* Test the result for 2916 atoms*/
 TEST(OUTPUT_RESULT2, REFRENCE_RESULT2)
 {
-    double tol = 10E-3;
-    std::string results_filename = "../examples/argon_2916.dat";
-    std::string reference_filename = "../reference/argon_2916.dat";
-
-    std::ifstream results_file(results_filename);
-    std::ifstream reference_file(reference_filename);
-
-
-    std::string results_line; 
-    std::string reference_line;
-
     std::vector<double> results_data;
     std::vector<double> reference_data;
 
-
-
-    /* Reading the results data*/
-    if(!results_file.is_open()){
-        std::cerr<<"There has been an issue opening the file"<<results_filename<<std::endl;
-    }
-    while(std::getline(results_file, results_line)){
-        std::istringstream iss(results_line);
-        double value;
-        while(iss >> value){
-            results_data.push_back(value);
-        }
-    }
-    results_file.close();
-
-    /*Reading the refrence data*/
-    if(!reference_file.is_open()){
-        std::cerr<<"There has been an issue opening the file"<<reference_filename<<std::endl;
-    }
-    while(std::getline(reference_file, reference_line)){
-        std::istringstream iss(reference_line);
-        double value;
-        while(iss >> value){
-            reference_data.push_back(value);
-        }
-    }
-    reference_file.close();
+    get_results(results_data, "../examples/argon_2916.dat");
+    get_results(reference_data, "../reference/argon_2916.dat");
 
     ASSERT_EQ(reference_data.size(), results_data.size());
-    size_t nrows{(reference_data.size() / 5) / 5000}; /*Checking the first 2000 rows*/
+    size_t nrows = reference_data.size() / 5; /*Checking all the results*/
     int ncols{5};
-    for(int i=0; i <  nrows;i++){
+    for (int i = 0; i < nrows; i++)
+    {
         for (int j = 0; j < ncols; j++)
         {
             EXPECT_NEAR(reference_data[i * 5 + j], results_data[i * 5 + j], tol)
-            <<"There tests fail at index i: "<<i<<" and j: "<<j;
+                << "There tests fail at index i: " << i << " and j: " << j;
         }
-        
     }
 }
 
 /* Test the result for 78732 atoms*/
 TEST(OUTPUT_RESULT3, REFRENCE_RESULT3)
 {
-    double tol = 10E-3;
-    std::string results_filename = "../examples/argon_78732.dat";
-    std::string reference_filename = "../reference/argon_78732.dat";
-
-    std::ifstream results_file(results_filename);
-    std::ifstream reference_file(reference_filename);
-
-
-    std::string results_line; 
-    std::string reference_line;
-
     std::vector<double> results_data;
     std::vector<double> reference_data;
 
-
-
-    /* Reading the results data*/
-    if(!results_file.is_open()){
-        std::cerr<<"There has been an issue opening the file"<<results_filename<<std::endl;
-    }
-    while(std::getline(results_file, results_line)){
-        std::istringstream iss(results_line);
-        double value;
-        while(iss >> value){
-            results_data.push_back(value);
-        }
-    }
-    results_file.close();
-
-    /*Reading the refrence data*/
-    if(!reference_file.is_open()){
-        std::cerr<<"There has been an issue opening the file"<<reference_filename<<std::endl;
-    }
-    while(std::getline(reference_file, reference_line)){
-        std::istringstream iss(reference_line);
-        double value;
-        while(iss >> value){
-            reference_data.push_back(value);
-        }
-    }
-    reference_file.close();
+    get_results(results_data, "../examples/argon_78732.dat");
+    get_results(reference_data, "../reference/argon_78732.dat");
 
     ASSERT_EQ(reference_data.size(), results_data.size());
-    size_t nrows{(reference_data.size() / 5) / 5000}; /*Checking the first 2000 rows*/
+    size_t nrows = reference_data.size() / 5; /*Checking all the results*/
     int ncols{5};
-    for(int i=0; i <  nrows;i++){
+    for (int i = 0; i < nrows; i++)
+    {
         for (int j = 0; j < ncols; j++)
         {
             EXPECT_NEAR(reference_data[i * 5 + j], results_data[i * 5 + j], tol)
-            <<"There tests fail at index i: "<<i<<" and j: "<<j;
+                << "There tests fail at index i: " << i << " and j: " << j;
         }
-        
     }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     // Initialize Google Test framework
     ::testing::InitGoogleTest(&argc, argv);
 
