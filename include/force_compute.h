0b5329d0f161f328ba4326d914f7f9b5af237a84
bellou <rand@random.it>
2024-11-28 15:54:59 +0100

    commneted on the ekin function

--- include/force_compute.h
+++ include/force_compute.h
@@ -5,16 +5,21 @@
 
 /**
    * @brief Computes the forces.
-   * @param[in] *sys `Struct pointer, containing the systems information (i.e. number of atoms, box dimensions, ...)`
+   * @param[in] *sys `mdsys_t struct pointer, containing the systems information (i.e. number of atoms, box dimensions, ...)`
+   * @note `The results are saved to a mdsys_t struct members: sys->fx, sys->fy, sys->fz`
    */
 void force(mdsys_t *sys);
-// Compute kinetic energy for all particles
+/**
+   * @brief Computes the kentic energies.
+   * @param[in] *sys `Struct pointer, containing the systems information (i.e. number of atoms, box dimensions, ...)`
+   */
 void ekin(mdsys_t *sys);
 /**
    * @brief Periodic boundary conditions.
    * @param[in] x `One dimensional coordinate of the i th particle (i.e. x, y, or z)`
    * @param[in] boxby2 `Box dimension in the given direction`
    * @param[out] x `New position of the i th particle`
+   * @note `The results are saved to a mdsys_t struct member sys->ekin`
    */
 static inline double pbc(double x, const double boxby2);
 
