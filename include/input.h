c5b4f33192dfcf55c1594ef80799a7adabfc1cd0
bellou <rand@random.it>
2024-11-28 15:59:58 +0100

    added comments to the get_a_line and read_input_files functions

--- include/input.h
+++ include/input.h
@@ -4,9 +4,19 @@
 #include <stdio.h>  
 #include "mdsys.h"  // struct definition header file
 
-// Read one line from the input file
+/**
+ * @brief Read a line from an input file
+ * @param[in] fp `File pointer`
+ * @param[in] buf `... pointer`
+ * @param[out] error message  
+ */
 static int get_a_line(FILE *fp, char *buf);
-// Read the entire files and initialize the system parameters from the input
+/**
+ * @brief Read all input files and intilize system paramters. 
+ * @param[in] sys `Struct to store the systems paramters`
+ * @param[in] ergfile `... pointer`
+ * @param[in] trajfile ``
+ */
 int read_input_files(mdsys_t *sys, FILE **ergfile, FILE **trajfile);
 
 #endif /* INPUT_H */
